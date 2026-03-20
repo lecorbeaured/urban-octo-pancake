75b57ce99e9422f97d85d7a8d237124d80a28468
Mollah <mollah@Mollahs-iMac.local>
2026-03-19 21:46:09 -0700

    Use React.lazy + Suspense for code-split pages to avoid TLA during build

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles.css'
@@ -7,20 +7,25 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Privacy from './pages/Privacy'
 import Disclosure from './pages/Disclosure'
+const CategoryPage = React.lazy(() => import('./pages/categories/CategoryPage'))
+const ProductPage = React.lazy(() => import('./pages/products/ProductPage'))
+const BestBeachChairs = React.lazy(() => import('./pages/guides/BestBeachChairs'))
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Suspense fallback={<div className='loader'>Loading…</div>}>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
-        <Route path='/category/:slug' element={React.createElement((await import('./pages/categories/CategoryPage')).default)} />
-        <Route path='/product/:slug' element={React.createElement((await import('./pages/products/ProductPage')).default)} />
-        <Route path='/guides/best-beach-chairs' element={React.createElement((await import('./pages/guides/BestBeachChairs')).default)} />
+        <Route path='/category/:slug' element={<CategoryPage/>} />
+        <Route path='/product/:slug' element={<ProductPage/>} />
+        <Route path='/guides/best-beach-chairs' element={<BestBeachChairs/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/privacy' element={<Privacy/>} />
         <Route path='/disclosure' element={<Disclosure/>} />
       </Routes>
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>
 )
